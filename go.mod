5f15f8022a63127821ddacff615c66797d4c21fd
tirpitz0509 <31152598+tirpitz0509@users.noreply.github.com>
2022-01-27 22:40:32 +0400

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-buzinessware
+
+go 1.16
---
+++ sendinblue.go
@@ -0,0 +1,47 @@
+package sendinblue
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type Sendinblue struct {
+	StatusCode int    `json:"statusCode"` //
+	MessageId  string `json:"messageId"`  //
+}
+
+var reqUrl = "https://api.sendinblue.com/v3/smtp/email"
+
+func Sendmail(request []byte, apiKey string) (Sendinblue, error) {
+	var t Sendinblue
+	method := "POST"
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, reqUrl, bytes.NewBuffer(request))
+	req.Header.Add("Api-Key", apiKey)
+	req.Header.Add("Content-type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := client.Do(req)
+
+	// check for response error
+	if err != nil {
+		return t, err
+	}
+	defer resp.Body.Close()
+
+	t.StatusCode = resp.StatusCode
+
+	if resp.StatusCode == 201 {
+		data := json.NewDecoder(resp.Body)
+		errjson := data.Decode(&t)
+		if errjson != nil {
+			return t, err
+		}
+	}
+	// close response body
+	resp.Body.Close()
+
+	return t, nil
+}
