556b2e941a186ac97e7947d5e203664a49a7e7e6
tirpitz0509 <31152598+tirpitz0509@users.noreply.github.com>
2022-01-28 14:49:06 +0400

    28-01-22

--- bwdataapi/bwdataapi.go
+++ bwdataapi/bwdataapi.go
@@ -1,9 +1,54 @@
 package bwdataapi
 
 import (
-	"fmt"
+	"bytes"
+	"errors"
+	"net/http"
 )
 
-func Hello() {
-	fmt.Println("Hello")
+var reqUrl = "https://api.buzinessware.com/"
+
+func PostRequest(request []byte, path string, method string, username string, password string) (*http.Response, error) {
+	var resp *http.Response
+
+	//Validation
+	validate := validator.New()
+	err := validate.Var(path, "required")
+	if err != nil {
+		return resp, errors.New("Path not provided")
+	}
+
+	err = validate.Var(method, "required")
+	if err != nil {
+		return resp, errors.New("Method not provided")
+	}
+
+	err = validate.Var(username, "required")
+	if err != nil {
+		return resp, errors.New("Username not provided")
+	}
+
+	err = validate.Var(password, "required")
+	if err != nil {
+		return resp, errors.New("Password not provided")
+	}
+
+	client := &http.Client{}
+
+	reqUrl = reqUrl + path
+
+	req, err := http.NewRequest(method, reqUrl, bytes.NewBuffer(request))
+	req.Header.Add("Content-type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.SetBasicAuth(username, password)
+
+	resp, err = client.Do(req)
+
+	// check for response error
+	if err != nil {
+		return resp, err
+	}
+	defer resp.Body.Close()
+
+	return resp, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/bwdxbfzco/go-buzinessware
 
 go 1.13
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+)
